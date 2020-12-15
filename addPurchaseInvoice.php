2c466da3f558d2035b793b929c3c42eac7091715
Kacper Zarnicki <k.zarnicki@gmail.com>
2020-12-15 19:30:05 +0100

    add purchase invoice

---
+++ addPurchaseInvoice.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["invoiceNumber"]; ?><br>
+
+
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$config = [];
+$config['dsn'] = 'mysql:dbname=id15055529_company; host=localhost';
+$config['username'] = 'root';
+$config['password'] = '';
