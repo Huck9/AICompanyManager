86f1d2a4df8357789dab559c78b365f3b41fb69f
kubatu <jtuzinek@gmail.com>
2020-12-31 16:22:24 +0100

    backup

--- licences.php
+++ licences.php
@@ -20,17 +20,44 @@
         #nazwykolumn {
             background-color: greenyellow;
         }
-        #naglowek{
+        #naglowek {
             text-align: center;
             font-size: 50px;
         }
         body {
             background-color: #fff3ea;
         }
+        #panellicence {
+            text-align: center;
+            padding-bottom: 5px;
+        }
+        button{
+            display:inline-block;
+            padding:0.3em 1.2em;
+            margin:0 0.3em 0.3em 0;
+            border-radius:2em;
+            box-sizing: border-box;
+            text-decoration:none;
+            font-family:'Roboto',sans-serif;
+            font-weight:300;
+            color:#FFFFFF;
+            background-color:#4eb5f1;
+            text-align:center;
+            transition: all 0.2s;
+        }
+        button:hover{
+            background-color:#4095c6;
+        }
     </style>
 </head>
 <body>
 <div id="naglowek">List of licences</div>
+<div id="panellicence">
+    <input type="text" id="searchinput">
+    <button id="searchlicence">Szukaj</button>
+    <button id="addlicence">Dodaj nowy</button>
+    <button id="editlicence">Edytuj</button>
+</div>
 <div id="tablelicence">
 <table>
     <tr id="nazwykolumn">
@@ -46,10 +73,9 @@
         <th>Notatki</th>
     </tr>
     <?php
-    $dsn = "mysql:host=localhost;dbname=id15055529_company";
-    $user = "root";
-    $passwd = "";
-    $pdo = new PDO($dsn, $user, $passwd);
+    require_once("config.php");
+    global $config;
+    $pdo = new PDO($config['dsn'], $config['username'], $config['password']);
     $stm = $pdo->query("SELECT * FROM licences");
     $rows = $stm->fetchALL(PDO::FETCH_ASSOC);
 
