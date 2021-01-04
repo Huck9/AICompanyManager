b75450dd883c14b89b16fdcd5cd49f6cddd0f080
kppic <kppichlinski@gmail.com>
2021-01-04 22:12:46 +0100

    change index from html to php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
         <div id="login_panel">
 
-            <form action="login.php" method="POST">
+            <form action="index.php" method="POST">
 
                 <div id="login-header">
                     <h1> M&M <br> Company Manager </h1>
@@ -53,3 +53,37 @@
 </body>
 
 </html>
+
+<?php
+
+require_once("config.php");
+global $config;
+
+$pdo = new PDO($config['dsn'], $config['username'], $config['password']);
+
+if (isset($_POST['sign'])) {
+
+    $login = $_POST['login'];
+    $password = md5($_POST['password']);
+
+    $stm = $pdo->query("SELECT * FROM users WHERE login='$login' AND password='$password' ");
+    $user = $stm->fetch();
+
+    if ($user > 0) {
+        $role = $_POST['account'];
+        session_start();
+        $_SESSION['name'] = $login;
+        $_SESSION['role'] = $role;
+        header("location: panel.html");
+        echo "Udalo sie zalogowac";
+    } else {
+        ?>
+        <script type="text/javascript">
+            document.getElementById("error").innerHTML = "<?php echo "Zły login lub hasło" ?>";
+        </script>
+        <?php
+    }
+}
+
+?>
+
